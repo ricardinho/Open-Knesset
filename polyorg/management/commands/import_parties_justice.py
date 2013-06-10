1d83e1710deee94cfc86a0568bad1dbd35c8cb76
Ricardo Moreno <parcero.cubano@gmail.com>
2013-06-10 20:22:00 +0300

    fixed bug in management command to import polyorg parties

--- polyorg/management/commands/import_parties_justice.py
+++ polyorg/management/commands/import_parties_justice.py
@@ -10,16 +10,16 @@ TABLE_ADDRESS = 'http://www.justice.gov.il/MOJHeb/RasutHataagidim/RashamMiflagot
 
 
 class Command(BaseCommand):
-    help = '''Import the list of polyorg parties 
+    help = '''Import the list of polyorg parties
         from the justice ministry website'''
-    
+
     def handle(self, *args, **kwargs):
         self.stdout.write('contacting justice ministry website')
         page = urllib2.urlopen(TABLE_ADDRESS)
         soup = BeautifulSoup(page.read())
         tbl = soup.find('table', 'MsoNormalTable')
         parser = HTMLParser()
-        for row in tbl.findAll('tr'):
+        for row in tbl.findAll('tr')[1:]:
             text = row.find('td').p.span.string
             if text:
                 Party.objects.get_or_create(
